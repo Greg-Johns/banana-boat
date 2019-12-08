1ed7cb2c0a7f6b8fe8c65f456fb70ee2a3de39a9
Greg Johns <Greg.johns@gmail.com>
2019-12-08 10:56:46 -0600

    git test

--- src/BoatCard.js
+++ src/BoatCard.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import { Subscribe } from 'unstated';
-import BoatsContainer from './BoatsContainer';
-
+import React, { Component } from 'react'
+import { Subscribe } from 'unstated'
+import BoatsContainer from './BoatsContainer'
 
 // Styles
-const enabledBlue = '#2FCDFF';
-const disabledBlue = '#5CD1F6';
+const enabledBlue = '#2FCDFF'
+const disabledBlue = '#5CD1F6'
 
 const s = {
   optsEnabled: {
@@ -16,7 +15,7 @@ const s = {
     zIndex: '100'
   },
   optsDisabled: {
-    backgroundColor: `${disabledBlue}`,
+    backgroundColor: `${disabledBlue}`
   },
   boatEnabled: {
     display: `inline-block`,
@@ -39,19 +38,19 @@ const s = {
     filter: 'drop-shadow(-1px 1px 1px #31788E)'
   },
   nanersEnabled: {
-    padding: "1px",
-    paddingLeft: "6px",
-    width: "28px",
-    border: "1px solid #ccc",
-    borderRadius: "4px"
+    padding: '1px',
+    paddingLeft: '6px',
+    width: '28px',
+    border: '1px solid #ccc',
+    borderRadius: '4px'
   },
   nanersDisabled: {
-    padding: "1px",
-    paddingLeft: "6px",
-    width: "28px",
-    backgroundColor: "#B2EDFF",
-    border: "0 solid #B2EDFF",
-    borderRadius: "4px"
+    padding: '1px',
+    paddingLeft: '6px',
+    width: '28px',
+    backgroundColor: '#B2EDFF',
+    border: '0 solid #B2EDFF',
+    borderRadius: '4px'
   },
   label: {
     color: '#666',
@@ -66,28 +65,28 @@ const s = {
 }
 
 class BoatCard extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     // Set range values for people & banana options
     this.state = {
-      peopleRange: [1,2,3,4,5,6]
-    };
-  }
-
-  componentWillMount() {
-    if (localStorage.getItem('boats')) {
-      let fromStorage = localStorage.getItem('boats');
-      this.setState({boats: fromStorage});
+      peopleRange: [1, 2, 3, 4, 5, 6]
     }
   }
 
-  render(props) {
+  componentWillMount () {
+    if (localStorage.getItem('boats')) {
+      let fromStorage = localStorage.getItem('boats')
+      this.setState({boats: fromStorage})
+    }
+  }
+
+  render (props) {
     // Options helper functions
     const peopleValues = this.state.peopleRange.map((num) =>
       <option key={num} value={num}>{num}</option>
-    );
+    )
 
-    let boatNum = this.props.num;
+    let boatNum = this.props.num
 
     return (
       <Subscribe to={[BoatsContainer]}>
@@ -97,8 +96,8 @@ class BoatCard extends Component {
             : s.boatDisabled}
           >
             <input
-              style={(boatNum === 0) ? {display: "none"} : {display: "inline"}}
-              type="checkbox"
+              style={(boatNum === 0) ? {display: 'none'} : {display: 'inline'}}
+              type='checkbox'
               checked={boatLoad.state.boats[boatNum].enabled}
               onChange={() =>
                 boatLoad.state.boats[boatNum].enabled
@@ -115,7 +114,7 @@ class BoatCard extends Component {
                 <label style={s.label}>People:</label>
                 <select
                   disabled={!boatLoad.state.boats[boatNum].enabled}
-                  name="peopleOps"
+                  name='peopleOps'
                   value={boatLoad.state.boats[boatNum].peopleCount}
                   onChange={(e) => boatLoad.setPeopleCount(boatNum, e.target.value)}
                 >
@@ -131,10 +130,10 @@ class BoatCard extends Component {
                     ? s.nanersEnabled
                     : s.nanersDisabled
                   }
-                  type="text"
-                  id="nana"
-                  name="nana"
-                  maxlength="3"
+                  type='text'
+                  id='nana'
+                  name='nana'
+                  maxlength='3'
                   value={boatLoad.state.boats[boatNum].bananaCount}
                   onChange={(e) => boatLoad.setBananaCount(boatNum, e.target.value)}
                 />
@@ -147,4 +146,4 @@ class BoatCard extends Component {
   }
 }
 
-export default BoatCard;
+export default BoatCard
